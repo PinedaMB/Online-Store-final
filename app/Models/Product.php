5127006cdd4c687cec117da5efa50db890b768fc
PinedaMB <mendez-pineda@outlook.es>
2022-12-07 07:50:16 -0500

    Shopping cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,6 +16,15 @@ class Product extends Model
         ]);
     }
 
+    public static function sumPricesByQuantities($products, $productsInSession)
+    {
+        $total = 0;
+        foreach ($products as $product) {
+            $total = $total + ($product->getPrice() * $productsInSession[$product->getId()]);
+        }
+        return $total;
+    }
+
     public function getId()
     {
         return $this->attributes['id'];
@@ -73,13 +82,13 @@ class Product extends Model
         $this->attributes['updated_at'] = $updatedAt;
     }
     /**
-    * PRODUCT ATTRIBUTES
-    * $this->attributes['id'] - int - contains the product primary key (id)
-    * $this->attributes['name'] - string - contains the product name
-    * $this->attributes['description'] - string - contains the product description
-    * $this->attributes['image'] - string - contains the product image
-    * $this->attributes['price'] - int - contains the product price
-    * $this->attributes['created_at'] - timestamp - contains the product creation date
-    * $this->attributes['updated_at'] - timestamp - contains the product update date
-    */
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product primary key (id)
+     * $this->attributes['name'] - string - contains the product name
+     * $this->attributes['description'] - string - contains the product description
+     * $this->attributes['image'] - string - contains the product image
+     * $this->attributes['price'] - int - contains the product price
+     * $this->attributes['created_at'] - timestamp - contains the product creation date
+     * $this->attributes['updated_at'] - timestamp - contains the product update date
+     */
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,12 @@ Route::middleware('admin')->group(function () {
     Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
 });
 
+/**
+ * Cart Routes
+ */
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/delete', 'App\Http\Controllers\CartController@delete')->name("cart.delete");
+Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
 
 //Route::post('publicacion/(publicacion)/editar/(id)')
 
